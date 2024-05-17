6a14dcc5407d272f95b9df06bb1e46c2170bff77
Harun KARACA <harunkaraca99@gmail.com>
2024-05-17 14:43:12 +0200

    utils implemented

---
+++ app/src/main/java/com/example/mybaseprojectwithhilt/util/ViewExt.kt
@@ -0,0 +1,39 @@
+package com.example.mybaseprojectwithhilt.util
+
+import android.view.View
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import com.google.android.material.snackbar.Snackbar
+
+/**
+ * Transforms static java function Snackbar.make() to an extension function on View.
+ */
+fun View.showSnackbar(snackbarText: String, timeLength: Int) {
+    Snackbar.make(this, snackbarText, timeLength).run {
+        addCallback(object : Snackbar.Callback() {
+            override fun onShown(sb: Snackbar?) {
+//                EspressoIdlingResource.increment()
+            }
+
+            override fun onDismissed(transientBottomBar: Snackbar?, event: Int) {
+//                EspressoIdlingResource.decrement()
+            }
+        })
+        show()
+    }
+}
+
+/**
+ * Triggers a snackbar message when the value contained by snackbarTaskMessageLiveEvent is modified.
+ */
+fun View.setupSnackbar(
+    lifecycleOwner: LifecycleOwner,
+    message: LiveData<String>,
+    timeLength: Int
+) {
+
+    message.observe(lifecycleOwner, Observer { event ->
+        showSnackbar(message.value!!, timeLength)
+    })
+}
