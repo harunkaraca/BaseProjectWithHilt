6dc714959d7fff6ad9ec899bfab0aa07ae41a0b8
Harun KARACA <harunkaraca99@gmail.com>
2024-05-17 14:42:16 +0200

    flagurl property added

--- app/src/main/java/com/example/mybaseprojectwithhilt/data/source/local/entity/CountryTable.kt
+++ app/src/main/java/com/example/mybaseprojectwithhilt/data/source/local/entity/CountryTable.kt
@@ -6,8 +6,9 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "country")
 data class CountryTable @JvmOverloads constructor(
-    @ColumnInfo(name = "country_name") var countryName: String? = "",
-    @ColumnInfo(name = "capital") var capital: String? = "",
+    @ColumnInfo(name = "country_name") var countryName: String = "",
+    @ColumnInfo(name = "capital") var capital: String = "",
+    @ColumnInfo(name = "flag_url") var flagUrl: String? = "",
     @PrimaryKey @ColumnInfo(name = "id") var id: Int = 0
 ) {
 
